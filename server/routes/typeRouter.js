1470dcbb87e8afd3a8705cca5d655b4a22399709
AlexSimakhin <alexsimahin@gmail.com>
2021-04-22 18:26:57 +0300

    add: (react app / server auth)

--- server/routes/typeRouter.js
+++ server/routes/typeRouter.js
@@ -3,8 +3,9 @@ const Router = require('express');
 const router = new Router();
 
 const typeContoller = require('../controllers/typeController');
+const checkRole = require('../middleware/checkRoleMiddleware');
 
-router.post('/', typeContoller.create);
+router.post('/', checkRole('ADMIN'), typeContoller.create);
 router.get('/', typeContoller.getAll);
 
 module.exports = router;
